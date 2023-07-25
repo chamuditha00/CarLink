28a824eb9690dcba25ee8aa3da89b3cb2a98ae66
Weerawi <weerawikramatharindu@gmail.com>
2023-07-25 16:22:27 +0530

    POst created error free

--- src/main/java/com/example/carlink/Service/UserService.java
+++ src/main/java/com/example/carlink/Service/UserService.java
@@ -21,6 +21,7 @@ public class UserService {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private ProfileRepository profileRepository;
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
     public Profile registerUser(Profile user) throws UserException {
